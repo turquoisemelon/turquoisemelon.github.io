dd393bf0c55eb83a713688630f89639b2d0ab7c5
Meltem <melltemkilic@gmail.com>
2018-12-25 16:58:18 -0500

    Add styled-components gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
   ],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from 'styled-components'
+import {Icon} from './icon'
 
 const StyledFooter = styled.div`
     display: flex;
